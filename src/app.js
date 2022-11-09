a69238060d16a7a2c06e73742a7087661a92b862
LuciaRyan <lcryan06@gmail.com>
2022-11-09 11:26:36 +0100

    started assignment, build async function fetchCountryData to get data from https://restcountries.com/v2/all

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 console.log('Hallo daar!');
+
+async function fetchCountryData() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all')
+        console.log(result);
+    }
+}
+
+fetchCountryData()

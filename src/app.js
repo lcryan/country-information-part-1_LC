92baa7735cd8aca9ea7bd6dfbe5d1d32382e4a31
LuciaRyan <lcryan06@gmail.com>
2022-11-09 15:18:19 +0100

    population under countrynames within the async function

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 console.log('Hallo daar!');
 const countryList = document.getElementById('list-of-countries')
+const populationData = document.getElementById('list-of-countries')
 
 async function fetchCountryData() {
     try {
@@ -9,13 +10,22 @@ async function fetchCountryData() {
         console.log(axiosResponse)
 
 
-        axiosResponse.data.map((countryName) => {
+        axiosResponse.data.map((country)  => {
+
             const countryNames = document.createElement('li');
             countryNames.setAttribute('class', 'countryName');
-            countryNames.textContent = countryName.name;
+            countryNames.textContent = country.name;
 
             countryList.appendChild(countryNames);
-        });
+
+            const populationC = document.createElement('li');
+            populationC.setAttribute('class','population');
+            populationC.textContent = `Has a population of ${country.population} people.`;
+
+            populationData.appendChild(populationC)
+
+            });
+
 
     } catch (e) {
         console.error(e);

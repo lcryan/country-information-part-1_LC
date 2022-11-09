9a8fa87a909c3da5bd8f61992c3033658fe35be4
LuciaRyan <lcryan06@gmail.com>
2022-11-09 13:53:44 +0100

    created first method within async function to retrieve all Countries from API

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import axios from 'axios';
 
 console.log('Hallo daar!');
+const countryList = document.getElementById('list-of-countries')
 
 async function fetchCountryData() {
     try {
         const axiosResponse = await axios.get('https://restcountries.com/v2/all')
         console.log(axiosResponse)
-        } catch (e) {
+
+
+        axiosResponse.data.map((countryName) => {
+            const countryNames = document.createElement('li');
+            countryNames.setAttribute('class', 'countryName');
+            countryNames.textContent = countryName.name;
+
+            countryList.appendChild(countryNames);
+        });
+
+    } catch (e) {
         console.error(e);
     }
 }
+
 fetchCountryData();
 

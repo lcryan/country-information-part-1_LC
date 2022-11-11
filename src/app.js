0011afffbeded2bb5c9ab9cc5362a757267e086a
LuciaRyan <lcryan06@gmail.com>
2022-11-11 20:26:45 +0100

    added HTML 2 for 2.assignment added extra css to not corrupt styles.css added extra png of globe to assets folder

--- src/app.js
+++ src/app.js
@@ -7,57 +7,64 @@ const populationData = document.getElementById('list-of-countries')
 
 
 async function fetchCountryData() {
+    let axiosResponse
     try {
-        const axiosResponse = await axios.get('https://restcountries.com/v2/all')
-        console.log(axiosResponse)
-
-
-        axiosResponse.data.map((country) => {
-
-            const countryNames = document.createElement('li');
-            countryNames.setAttribute('class', 'countryName');
-            countryNames.textContent = country.name;
-            countryList.appendChild(countryNames);
-
-
-
-            const flagC = document.createElement('img')
-            flagC.setAttribute('id', 'flag')
-            flagC.setAttribute('src', country.flags.png);
-            countryFlag.appendChild(flagC);
-
-            const populationC = document.createElement('li');
-            populationC.setAttribute('class', 'population');
-            populationC.textContent = `Has a population of ${country.population} people`;
-            populationData.appendChild(populationC);
-
-
-
-            switch (country.region) {
-                case "Africa":
-                    countryNames.setAttribute('id', 'africa');
-                    break
-                case "Americas":
-                    countryNames.setAttribute('id', 'americas');
-                    break
-                case "Asia":
-                    countryNames.setAttribute('id', 'asia');
-                    break
-                case "Europe":
-                    countryNames.setAttribute('id', 'europe');
-                    break
-                case "Oceania":
-                    countryNames.setAttribute('id', 'oceania');
-                    break
-                default:
-                    countryNames.setAttribute('id', 'other');
-            }
-        })
-
-
+        axiosResponse = await axios.get('https://restcountries.com/v2/all')
     } catch (e) {
         console.error(e);
     }
+    console.log(axiosResponse)
+
+    const sortedCountries = axiosResponse.data.sort((a, b) => {
+        return a.population - b.population
+    })
+
+    const countries = sortedCountries.map((country) => {
+        const countryNamesContainer = document.createElement('li');
+        /*countryNamesContainer.setAttribute('class', 'countryName');*/
+
+        const countryName = document.createElement('span')
+        countryName.textContent = country.name;
+        countryName.setAttribute('class', 'countryName')
+        countryNamesContainer.appendChild(countryName);
+
+        const flagC = document.createElement('img');
+        flagC.setAttribute('id', 'flag');
+        flagC.setAttribute('src', country.flags.png);
+        countryNamesContainer.appendChild(flagC);
+
+        const populationC = document.createElement('p');
+        populationC.setAttribute('class', 'population');
+        populationC.textContent = `Has a population of ${country.population} people`;
+        countryNamesContainer.appendChild(populationC);
+
+        switch (country.region) {
+            case "Africa":
+                countryName.setAttribute('id', 'africa');
+                break
+            case "Americas":
+                countryName.setAttribute('id', 'americas');
+                break
+            case "Asia":
+                countryName.setAttribute('id', 'asia');
+                break
+            case "Europe":
+                countryName.setAttribute('id', 'europe');
+                break
+            case "Oceania":
+                countryName.setAttribute('id', 'oceania');
+                break
+            default:
+                countryName.setAttribute('id', 'other');
+        }
+
+        return countryNamesContainer;
+
+    })
+
+    countryList.append(...countries);
+
+
 }
 
 fetchCountryData();
